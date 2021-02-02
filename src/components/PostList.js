7673bf6a6610aca435637ec13afd0267325a3720
HereComePretzel <austinoie@gmail.com>
2021-02-02 15:22:22 -0600

    further setup

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
 
 class PostList extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+
+
     render() {
         return (
             <div>Post List</div>
@@ -10,4 +17,5 @@ class PostList extends React.Component {
     }
 }
 
-export default PostList
+
+export default connect(null, {fetchPosts})(PostList)
